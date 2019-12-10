c4202ebd292cf502ba94cd20badfd2f37bf50cc0
g7 <123456>
2019-12-10 10:56:45 +0800

    merge

--- parser/port/parse.go
+++ parser/port/parse.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package port
 
 import (
@@ -18,24 +17,3 @@ func Parse(s *smbios.Structure) (*Information, error) {
 	}
 	return info, nil
 }
-=======
-package port
-
-import (
-	"dmidecode/smbios"
-)
-
-// Parse 解析port信息
-func Parse(s *smbios.Structure) (*Information, error) {
-	data := s.Formatted
-	info := &Information{
-		Header:                      s.Header,
-		InternalReferenceDesignator: s.GetString(0x0),
-		InternalConnectorType:       ConnectorType(data[0x01]),
-		ExternalReferenceDesignator: s.GetString(0x2),
-		ExternalConnectorType:       ConnectorType(data[0x03]),
-		Type:                        PortType(data[0x04]),
-	}
-	return info, nil
-}
->>>>>>> fd60aa940a50299abe4322cdf257a0501b993f04
