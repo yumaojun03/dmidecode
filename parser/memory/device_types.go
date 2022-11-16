5e0a150603300ecc12b94e5c1a27c93d76391d57
danil_e71 <danile71@gmail.com>
2022-11-16 14:23:09 +0300

    add new mem types

--- parser/memory/device_types.go
+++ parser/memory/device_types.go
@@ -82,6 +82,7 @@ const (
 
 func (m MemoryDeviceType) String() string {
 	types := [...]string{
+		"",
 		"Other",
 		"Unknown",
 		"DRAM",
@@ -112,10 +113,21 @@ func (m MemoryDeviceType) String() string {
 		"LPDDR2",
 		"LPDDR3",
 		"LPDDR4",
+		"Logical non-volatile device",
+		"HBM (High Bandwidth Memory)",
+		"HBM2 (High Bandwidth Memory Generation 2)",
+		"DDR5",
+		"LPDDR5",
+		"HBM3 (High Bandwidth Memory Generation 3)",
 	}
-	return types[m-1]
+	if len(types) <= int(m) {
+		return "Unknown"
+	}
+	return types[m]
 }
 
+// ValueMap={0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37..32567, 32568..65535}
+// Values={Unknown, Other, DRAM, Synchronous DRAM, Cache DRAM, EDO, EDRAM, VRAM, SRAM, RAM, ROM, Flash, EEPROM, FEPROM, EPROM, CDRAM, 3DRAM, SDRAM, SGRAM, RDRAM, DDR, DDR-2, BRAM, FB-DIMM, DDR3, FBD2, DDR4, LPDDR, LPDDR2, LPDDR3, LPDDR4, Logical non-volatile device, HBM (High Bandwidth Memory), HBM2 (High Bandwidth Memory Generation 2), DDR5, LPDDR5, HBM3 (High Bandwidth Memory Generation 3), DMTF Reserved, Vendor Reserved}
 type MemoryDeviceTypeDetail uint16
 
 const (
